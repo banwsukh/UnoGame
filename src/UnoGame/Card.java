99e948f0fd3c8bc8ab7cb0fb090f140e386858f4
banwsukh <ssbanwait78@gmail.com>
2020-02-05 18:31:39 -0500

    added classes through VP

---
+++ src/UnoGame/Card.java
@@ -0,0 +1,4 @@
+package UnoGame;
+
+public class Card {
+}
---
+++ src/UnoGame/GamePlayer.java
@@ -0,0 +1,17 @@
+package UnoGame;
+
+/**
+ * @author vicba
+ */
+public class GamePlayer {
+
+	/**
+	 * 
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO - implement GamePlayer.main
+		throw new UnsupportedOperationException();
+	}
+
+}
